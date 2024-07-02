9f11a03024f35da4798b87c51598d6317ed076de
Anton <53652055+from-lamer-to-programmer@users.noreply.github.com>
2024-07-02 07:20:24 +0300

    Update index.php

--- 5task/index.php
+++ 5task/index.php
@@ -205,7 +205,7 @@ else {
 
     try {
       $inQuery = implode(',', array_fill(0, count($selectedLangs), '?'));
-      $dbLangs = $db->prepare("SELECT id, language_name FROM languages WHERE language_name IN ($inQuery)");
+      $dbLangs = $db->prepare("SELECT id, language_name FROM Languages WHERE language_name IN ($inQuery)");
       foreach ($selectedLangs as $key => $value) {
         $dbLangs->bindValue(($key+1), $value);
       }
